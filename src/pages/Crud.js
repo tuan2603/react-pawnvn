dd79b66c47d46e9c9347e75eacd480edbe3d0001
Trần minh tuấn <tuan260387@gmail.com>
2018-06-25 22:40:31 +0700

    aaa

--- src/pages/Crud.js
+++ src/pages/Crud.js
@@ -4,12 +4,14 @@ import '../App.css';
 import ProductItem from './CrudItem';
 import ProductAdd from './CrudAdd';
 import { getFromStorage, setInStorage } from '../utils/storage';
+import socketIOClient from 'socket.io-client';
 
 class CRUD extends Component {
     constructor(props){
         super(props);
         this.state = {
-            products:[]
+            products:[],
+            endpoint: "http://125.212.216.80:8080",
         }
 
         this.onDelete = this.onDelete.bind(this);
@@ -24,7 +26,16 @@ class CRUD extends Component {
         }
     }
 
+    // method for emitting a socket.io event
+    send = () => {
+        const socket = socketIOClient(this.state.endpoint);
 
+        // this emits an event to the socket (your server) with an argument of 'red'
+        // you can make the argument any color you would like, or any kind of data you want to send.
+
+        socket.emit('change color', 'red');
+        // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+    }
 
 
     onDelete(name){
@@ -62,10 +73,18 @@ class CRUD extends Component {
     }
 
     render() {
-
+        const socket = socketIOClient(this.state.endpoint);
+        socket.on('change color', (color) => {
+            // setting the color of our button
+            document.body.style.backgroundColor = color;
+        })
         return (
             <div className="CRUD">
+
                 <h1>Products manager</h1>
+                <div style={{ textAlign: "center" }}>
+                    <button onClick={() => this.send()}>Change Color</button>
+                </div>
                 <ProductAdd
                     onAdd={this.onAdd}
                 />
